a5036df27bd8ca381dffc89c3e27579759c3fdd1
sturzhanova <Sturzhanova@gmail.com>
2022-12-20 05:02:46 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body style="font-family:Verdana;">
+
+    <div style="background-color:#f1f1f1;padding:15px;">
+        <h1>История модели Тойота Камри</h1>
+        <h3>Resize </h3>
+    </div>
+
+    <div style="overflow:auto">
+        <div class="menu">
+            <div class="menuitem"><a href="http://localhost/web3/index.php">About</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index1.php">About Complectation</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index2.php">About Manufacturer</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index3.php">About European</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index4.php">About American</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index5.php">About Arabian</a></div>
+            <div class="menuitem"><a href="http://localhost/web3/index6.php">Comment</a></div>
+
+
+        </div>
+
+        <div class="main">
+            <div class="flex">
+                <div class="divbox"> <img src="https://arhiv6.ufa-lib.ru/assets/images/21/vseznaika.png" class='img1'></div>
+                <div class="divbox">
+                    <h2 id='h21'> Почемy назвали Камри?</h2>
+                    <p id='p1'>
+                        Картинки по запросу почему назвали камри
+                        Название «Camry» происходит от японской фонетической записи китайского иероглифа 冠 (кáнмури), который имеет значение «корона».</p>
+                </div>
+            </div>
+            <div class="flex2">
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 1 поколение (V10) </h2>
+                        <h2 id='h21'>1982–1986</h2>
+                        <p id='p1'>
+                            Первое поколение автомобиля Toyota Camry было представлено в Японии в 1982 году, вскоре начался экспорт в США и Европу. </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_1.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 2 поколение (V20) </h2>
+                        <h2 id='h21'> 1986–1992</h2>
+                        <p id='p1'>
+                            В 1986 появилась Camry второго поколения. Она выпускалась на заводах в Японии, США и Австралии с кузовами седан и универсал. </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_2.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 3 поколение (V40, XV20) </h2>
+                        <h2 id='h21'> 1994–2001</h2>
+                        <p id='p1'>
+                            Седан Toyota Camry пятого поколения, хорошо известный и в России, выпускался с 2001 до 2006 года только с кузовом седан. </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_4.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 5 поколение (XV30) </h2>
+                        <h2 id='h21'> 2001–2006</h2>
+                        <p id='p1'>
+                            Первое поколение автомобиля Toyota Camry было представлено в Японии в 1982 году, вскоре начался экспорт в США и Европу. </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_5.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+                <br>
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 6 поколение (XV40) </h2>
+                        <h2 id='h21'> 2006–2011</h2>
+                        <p id='p1'>
+                            Шестое поколение модели было представлено в 2006 году, а в 2007 году стартовала сборка седанов «Камри» . </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_6.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+                <div class="flex1">
+                    <div class="divbox">
+                        <h2 id='h21'> 7 поколение (xv50) </h2>
+                        <h2 id='h21'> 2011-2017</h2>
+                        <p id='p1'>
+                            Седан бизнес-класса «Тойота Камри» (Toyota Camry) оснащается одним из трех бензиновых моторов </p>
+                    </div>
+                    <div class="divbox">
+                        <img src="https://wroom.ru/i/cars2/toyota_camry_7.jpg" class='img1'>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="flex">
+                <div class="divbox"> <img src="https://www.pngmart.com/files/10/Toyota-Camry-Transparent-Background.png" class='img1'></div>
+                <div class="divbox">
+                    <h2 id='h21'> 8 поколение (xv70)</h2>
+                    <h2 id='h21'>
+
+                        Купить Toyota Camry VIII (XV70) по цене от 1 560 000 рублей - более 1 468 Тойота Камри VIII (XV70)
+                        Toyota Camry VIII (XV70) – седан D-класса, передний и полный привод. Автомат и вариатор. Бензиновые и гибридные двигатели мощностью от 150 до 301 лошадиных сил.</p>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+
+
+
+</body>
+
+</html>
